f9602267dc6012c83d3e106a2563a8fc219931c5
toxicbakery <toxicbakery@gmail.com>
2014-11-04 23:54:55 -0500

    Added new cluster render for custom coloring of clusters, tweaked bike path coloring, and updated bike parking icons with a slight style tweak.

--- app/src/main/java/com/Orlando/opensource/bikeorlando/controller/BikeRackClusterManager.java
+++ app/src/main/java/com/Orlando/opensource/bikeorlando/controller/BikeRackClusterManager.java
@@ -2,14 +2,9 @@ package com.Orlando.opensource.bikeorlando.controller;
 
 import android.content.Context;
 
-import com.Orlando.opensource.bikeorlando.R;
 import com.Orlando.opensource.bikeorlando.data.BikeRackItem;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.clustering.*;
 
 public class BikeRackClusterManager extends ClusterManager<BikeRackItem> {
 
@@ -18,24 +13,6 @@ public class BikeRackClusterManager extends ClusterManager<BikeRackItem> {
 
         BikeRackClusterRenderer bikeRackClusterRenderer = new BikeRackClusterRenderer(context, map, this);
         setRenderer(bikeRackClusterRenderer);
-
-    }
-
-    private static class BikeRackClusterRenderer extends DefaultClusterRenderer<BikeRackItem> {
-
-        private final BitmapDescriptor bitmapDescriptor;
-
-        public BikeRackClusterRenderer(Context context, GoogleMap map,
-                                       ClusterManager<BikeRackItem> clusterManager) {
-            super(context, map, clusterManager);
-
-            bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.bikerackpinpoint);
-        }
-
-        @Override
-        protected void onBeforeClusterItemRendered(BikeRackItem item, MarkerOptions markerOptions) {
-            markerOptions.icon(bitmapDescriptor);
-        }
 
     }
 
